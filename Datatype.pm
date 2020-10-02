2e3e2065590a82a3e93ba21a86626a72a9bf68d7
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-10-03 01:03:13 +0200

    Remove obsolete code

--- Datatype.pm
+++ Datatype.pm
@@ -7,19 +7,6 @@ use Class::Utils qw(set_params);
 
 our $VERSION = 0.01;
 
-# Constructor.
-sub new {
-	my ($class, @params) = @_;
-
-	# Create object.
-	my $self = bless {}, $class;
-
-	# Process parameters.
-	set_params($self, @params);
-
-	return $self;
-}
-
 1;
 
 __END__
