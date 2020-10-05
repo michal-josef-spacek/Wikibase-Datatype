8f7be0f818d97fb524b8120da80f02e4fe05dd53
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-10-05 09:44:56 +0200

    Add doc for Wikidata::Datatype

--- Datatype.pm
+++ Datatype.pm
@@ -8,3 +8,83 @@ our $VERSION = 0.01;
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Wikidata::Datatype - Wikidata datatypes.
+
+=head1 DESCRIPTION
+
+This distributions is set of datatypes for Wikidata.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Wikidata::Datatype::Reference>
+
+Wikidata reference datatype
+
+=item L<Wikidata::Datatype::Sitelink>
+
+Wikidata sitelink datatype
+
+=item L<Wikidata::Datatype::Snak>
+
+Wikidata snak datatype
+
+=item L<Wikidata::Datatype::Statement>
+
+Wikidata statement datatype
+
+=item L<Wikidata::Datatype::Utils>
+
+Wikidata datatype utilities
+
+=item L<Wikidata::Datatype::Value>
+
+Wikidata value datatype
+
+=item L<Wikidata::Datatype::Value::Item>
+
+Wikidata item value datatype
+
+=item L<Wikidata::Datatype::Value::Monolingual>
+
+Wikidata monolingual value datatype
+
+=item L<Wikidata::Datatype::Value::String>
+
+Wikidata string value datatype
+
+=item L<Wikidata::Datatype::Value::Time>
+
+Wikidata time value datatype
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Wikidata-Datatype>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© Michal Josef Špaček 2020
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
