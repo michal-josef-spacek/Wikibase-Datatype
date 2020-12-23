058dcff87130244480492282add37513c0f1751e
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-12-23 15:41:38 +0100

    Prepare for 0.06 version

--- Datatype/Snak.pm
+++ Datatype/Snak.pm
@@ -33,7 +33,7 @@ Readonly::Array our @SNAK_TYPES => qw(
 	value
 );
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 has datatype => (
 	is => 'ro',
@@ -267,6 +267,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
