8b7032bf1c804f500ef1d69aea048e88cb8d4056
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-05-29 19:30:02 +0200

    Improve test instance for Human item

--- lib/Test/Shared/Fixture/Wikibase/Datatype/Item/Wikidata/Human.pm
+++ lib/Test/Shared/Fixture/Wikibase/Datatype/Item/Wikidata/Human.pm
@@ -4,8 +4,10 @@ use base qw(Wikibase::Datatype::Item);
 use strict;
 use warnings;
 
+use Test::Shared::Fixture::Wikibase::Datatype::Statement::Wikidata::GivenName::Douglas;
 use Test::Shared::Fixture::Wikibase::Datatype::Statement::Wikidata::InstanceOf::Human;
 use Test::Shared::Fixture::Wikibase::Datatype::Statement::Wikidata::SexOrGender::Male;
+use Wikibase::Datatype::Value::Monolingual;
 
 our $VERSION = 0.12;
 
@@ -13,13 +15,31 @@ sub new {
 	my $class = shift;
 
 	my @params = (
+		'aliases' => [
+			Wikibase::Datatype::Value::Monolingual->new(
+				'language' => 'en',
+				'value' => 'Douglas Noel Adams',
+			),
+		],
+		'descriptions' => [
+			Wikibase::Datatype::Value::Monolingual->new(
+				'language' => 'en',
+				'value' => 'English writer and humorist (1952-2001)',
+			),
+		],
+		'labels' => [
+			Wikibase::Datatype::Value::Monolingual->new(
+				'language' => 'en',
+				'value' => 'Douglas Adams',
+			),
+		],
+		'lastrevid' => 1645190860,
 		'statements' => [
 			Test::Shared::Fixture::Wikibase::Datatype::Statement::Wikidata::InstanceOf::Human->new,
 			Test::Shared::Fixture::Wikibase::Datatype::Statement::Wikidata::SexOrGender::Male->new,
+			Test::Shared::Fixture::Wikibase::Datatype::Statement::Wikidata::GivenName::Douglas->new,
 		],
-#		'lastrevid' => 1428556087,
-#		'title' => 'Q42',
-		# TODO
+		'title' => 'Q42',
 	);
 
 	my $self = $class->SUPER::new(@params);
