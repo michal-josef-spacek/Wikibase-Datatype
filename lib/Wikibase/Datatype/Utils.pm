3820f08e26c851875e52f1bd1236cea0df84d71d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-01-01 22:07:11 +0100

    Prepare for 0.37 version

--- lib/Wikibase/Datatype/Utils.pm
+++ lib/Wikibase/Datatype/Utils.pm
@@ -13,7 +13,7 @@ use Readonly;
 Readonly::Array our @EXPORT_OK => qw(check_datetime check_entity check_language
 	check_lexeme check_property check_sense);
 
-our $VERSION = 0.36;
+our $VERSION = 0.37;
 
 sub check_datetime {
 	my ($self, $key) = @_;
@@ -497,6 +497,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.36
+0.37
 
 =cut
